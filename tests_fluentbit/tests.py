2feef0dc41f94b35dea15bc340562c1dc88ed71f
Gael Pasgrimaud <gael@gawel.org>
2019-11-18 17:28:01 +0100

    use Forward everywhere

--- tests_fluentbit/tests.py
+++ tests_fluentbit/tests.py
@@ -1,10 +1,10 @@
 from fluent import sender
 
 
-logger = sender.FluentSender('app', host='fluentd', port=24224)
+logger = sender.FluentSender('Forward', host='fluentd', port=24224)
 print('Sender:', logger)
 
-res = logger.emit('follow', {'from': 'userA', 'to': 'userB'})
+res = logger.emit('Forward', {'from': 'userA', 'to': 'userB'})
 print('Result:', res)
 if not res:
     raise RuntimeError(logger.last_error)
