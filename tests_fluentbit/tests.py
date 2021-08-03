8587d28f3f6969241b5325ddea31438a50c0d914
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-08-03 17:12:12 +0200

    Fix: tests

--- tests_fluentbit/tests.py
+++ tests_fluentbit/tests.py
@@ -1,11 +1,11 @@
 from fluent import sender
 
 
-logger = sender.FluentSender('Forward', host='fluentd', port=24224)
-print('Sender:', logger)
+logger = sender.FluentSender("app", host="fluentd", port=24224)
+print("Sender:", logger)
 
-res = logger.emit('Forward', {'from': 'userA', 'to': 'userB'})
-print('Result:', res)
+res = logger.emit("test", {"from": "userA", "to": "userB"})
+print("Result:", res)
 if not res:
     raise RuntimeError(logger.last_error)
 
