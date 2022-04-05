b3937f982381490e968f3384f48abc0220f8826b
Gael Pasgrimaud <gael@gawel.org>
2022-04-05 15:04:43 +0200

    ignore certificate

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 set -eux;
 
 cd /usr/src/ || false
-curl "https://fluentbit.io/releases/${FLUENTBIT_MAJOR_VERSION}/fluent-bit-${FLUENTBIT_VERSION}.tar.gz" | tar -xvz
+curl -k "https://fluentbit.io/releases/${FLUENTBIT_MAJOR_VERSION}/fluent-bit-${FLUENTBIT_VERSION}.tar.gz" | tar -xvz
 
 cd "/usr/src/fluent-bit-${FLUENTBIT_VERSION}" || false
 ./debian.sh
